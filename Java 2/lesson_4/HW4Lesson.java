6ac7fa22d90c561c6ebedb3b2fe7d1700093e276
Igor Nalivnyh <nalivnyh@gmail.com>
2018-10-05 15:33:58 +0300

    edit

---
+++ Java 2/lesson_4/HW4Lesson.java
@@ -0,0 +1,20 @@
+package lesson_4;
+
+import javax.swing.*;
+
+/**
+ * GeekBrains. Java. Level 2. Lesson 4. HomeWork
+ *
+ * @author Igor Nalivnyh
+ * @version 05.10.2018
+ */
+
+public class hw4 {
+    JTextArea incoming;
+    JTextField outgoing;
+
+    public static void main(String[] args) {
+        
+    }
+
+}

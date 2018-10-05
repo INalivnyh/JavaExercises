66f1584c5cf3314c5ca074ddadc2c6a3f80f667a
Igor Nalivnyh <nalivnyh@gmail.com>
2018-10-05 16:58:26 +0300

    +

---
+++ HeadFirstJava/simpleGui/ch12_gui_intro/MyDrawPanel.java
@@ -0,0 +1,29 @@
+package SimpleGui1.ch12_gui_intro;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * GeekBrains. Java. Level 2. Lesson NUM. HomeWork
+ *
+ * @author Igor Nalivnyh
+ * @version 04.10.2018
+ */
+
+public class MyDrawPanel extends JPanel {
+    public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    MyDrawPanel panel = new MyDrawPanel();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(panel);
+    frame.setSize(300,300);
+    frame.setVisible(true);
+}
+
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.orange);
+        g.fillRect(100,50,100,100);
+    }
+
+
+}
---
+++ HeadFirstJava/simpleGui/ch12_gui_intro/SimpleGui1B.java
@@ -0,0 +1,36 @@
+package SimpleGui1.ch12_gui_intro;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Igor Nalivnyh
+ * @version 04.10.2018
+ */
+
+public class SimpleGui1B implements ActionListener {
+    JButton button;
+
+    public static void main(String[] args) {
+        SimpleGui1B gui = new SimpleGui1B();
+        gui.go();
+    }
+
+    public void go(){
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+
+        button.addActionListener(this);
+
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,200);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("I ve been clicked!");
+    }
+}

d3e9c87e049cff1ff07bb87c75fd90d38eb9861f
Igor Nalivnyh <nalivnyh@gmail.com>
2018-10-14 15:15:17 +0300

    +

---
+++ Java 2/lesson_6/ClassWork/ex2/Client.java
@@ -0,0 +1,77 @@
+package ex2;
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    private final static String address = "localhost";
+
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public static void main(String[] args) {
+
+        new Client();
+    }
+
+    private Client() {
+
+        try {
+            socket = new Socket(address, Server.PORT);
+            BufferedReader message = new BufferedReader(new InputStreamReader(System.in));
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            // поток для получения сообщений
+            Thread receiveMessagesThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("END")){
+                            System.out.println(str);
+                            break;
+                        }
+                        System.out.println(str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }  finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.exit(0);
+                }
+            });
+            receiveMessagesThread.start();
+
+            // поток для отправки сообщений
+            Thread sendMessagesThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String answer = message.readLine();
+                            out.writeUTF("Клиент написал" + ": " + answer.trim());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            sendMessagesThread.setDaemon(true);
+            sendMessagesThread.start();
+
+            try {
+                receiveMessagesThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Java 2/lesson_6/ClassWork/ex2/Server.java
@@ -0,0 +1,81 @@
+package ex2;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {;
+
+    public static void main(String[] args) {
+        new Server();
+    }
+
+    public final static int PORT = 5223;
+
+    public Server() {
+
+        try {
+            ServerSocket server = new ServerSocket(PORT);
+            Socket socket = server.accept();
+            System.out.println("Клиент подключился");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            BufferedReader message = new BufferedReader(new InputStreamReader(System.in));
+
+            // поток для получения сообщений
+            Thread receiveMessages = new Thread(() -> {
+                try {
+                    while (true) { //
+                        String str = in.readUTF();
+                        String[] test = str.split(" ");
+                        String end = test[2];
+                        boolean isEND = end.equals("END");
+                        if (isEND) {
+                            out.writeUTF("Завершение работы сервера");
+                            break;
+                        }
+                        System.out.println(str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                        out.close();
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            receiveMessages.start();
+
+            // поток для отправки сообщений
+            Thread sendMessagesThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String answer = message.readLine();
+                            out.writeUTF("Сервер написал" + ": " + answer.trim());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            sendMessagesThread.setDaemon(true);
+            sendMessagesThread.start();
+
+            try {
+                receiveMessages.join();
+            } catch (InterruptedException e) {
+                System.out.println("Прерывание потока пошло не по плану");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

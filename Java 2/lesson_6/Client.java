052ca51e534514fec17b2d086699b35e1ffe13fb
Igor Nalivnyh <nalivnyh@gmail.com>
2018-10-14 14:22:03 +0300

    +

---
+++ Java 2/lesson_6/Client.java
@@ -0,0 +1,85 @@
+package lesson_6;
+
+/**
+ * GeekBrains. Java. Level 2. Lesson 6. HomeWork
+ *
+ * @author Igor Nalivnyh
+ * @version 014.10.2018
+ */
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private final static String address = "localhost";
+
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public static void main(String[] args) {
+        new Client();
+    }
+
+    private Client() {
+
+        try {
+            socket = new Socket(address, Server.PORT);
+            BufferedReader message = new BufferedReader(new InputStreamReader(System.in));
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            //поток для получения сообщений
+            Thread receiveMessagesThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("END")) {
+                                System.out.println(str);
+                                break;
+                            }
+                            System.out.println(str);
+                        }
+                    } catch(IOException e){
+                        e.printStackTrace();
+
+                    }finally{
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        System.exit(0);
+                    }
+                });
+            receiveMessagesThread.start();
+
+            //поток для отправки сообщений
+                Thread sendMessagesThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while (true) {
+                                String answer = message.readLine();
+                                out.writeUTF("Клиент написал" + ": " + answer.trim());
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                });
+                sendMessagesThread.setDaemon(true);
+                sendMessagesThread.start();
+
+                try {
+                    receiveMessagesThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
